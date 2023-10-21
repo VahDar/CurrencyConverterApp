c911288c7231ecb230d7b7598596d6cc3b74d894
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-10-21 17:11:16 +0300

    add realm and create wallet model

---
+++ CurrencyConverterApp/Model/WalletModel.swift
@@ -0,0 +1,27 @@
+//
+//  WalletModel.swift
+//  CurrencyConverterApp
+//
+//  Created by Vakhtang on 21.10.2023.
+//
+
+import Foundation
+import RealmSwift
+
+class WalletModel: Object {
+    @Persisted(primaryKey: true) var id = UUID().uuidString
+    @Persisted var code: String = ""
+    @Persisted var amount: Double = 0.0
+    @Persisted var usdAmount: Double = 0.0
+    
+    var formattedAmount: String {
+        return String(format: "%.2f", amount)
+    }
+    
+    convenience init(code: String, amount: Double, usdAmount: Double) {
+        self.init()
+        self.amount = amount
+        self.code = code
+        self.usdAmount = usdAmount
+    }
+}
