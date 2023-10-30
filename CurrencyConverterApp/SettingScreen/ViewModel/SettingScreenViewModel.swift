1af1eaad82e78be3c31f215d2579510647efd2b1
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-10-30 18:00:57 +0200

    start setting viewmodel

---
+++ CurrencyConverterApp/SettingScreen/ViewModel/SettingScreenViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SettingScreenViewModel.swift
+//  CurrencyConverterApp
+//
+//  Created by Vakhtang on 30.10.2023.
+//
+
+import Foundation
+
+protocol SettingScreenViewModelProtocol {
+    
+}
+
+class SettingScreenViewModel {
+    
+}
