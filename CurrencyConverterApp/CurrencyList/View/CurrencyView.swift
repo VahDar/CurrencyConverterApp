c054f9901ce526c2c9e76a504f70285f0d58bd1a
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-11-05 17:27:08 +0200

    refactoring currencylist

--- CurrencyConverterApp/CurrencyList/View/CurrencyView.swift
+++ CurrencyConverterApp/CurrencyList/View/CurrencyView.swift
@@ -8,37 +8,38 @@
 import SwiftUI
 
 struct CurrencyView: View {
+    
     //MARK: - Properties
     @StateObject var viewModel = CurrencyListViewModel()
     @State private var isUserSearching = false
-        
+    
     var body: some View {
-            NavigationView {
-                VStack {
-                    SearchBarView(searchText: $viewModel.search, tools: [.search], title: "Currency List")
-                        .navigationBarHidden(true)
-                    if viewModel.showWarningMessage {
-                        VStack {
-                            Spacer()
-                            WarningView { Task {
-                                viewModel.fetchData()
-                            }
+        NavigationView {
+            VStack {
+                SearchBarView(searchText: $viewModel.search, tools: [.search], title: "Currency List")
+                    .navigationBarHidden(true)
+                if viewModel.showWarningMessage {
+                    VStack {
+                        Spacer()
+                        WarningView { Task {
+                            viewModel.fetchData()
+                        }
                         }
                     }
                 }
-                    List {
-                        ForEach(viewModel.searchFilter.sorted(by: { $0.key < $1.key }), id: \.key) { (currencyCode, rate) in
-                            CurrencyCellView(imageAndName: currencyCode, currencyName: viewModel.countryCurrenciesManager.findCurrencyName(for: currencyCode) ?? "", amount: rate, selectedCurrency: viewModel.savedName)                        }
-                    }
-                    .listStyle(.plain)
-                    .listRowBackground(Color.clear)
+                List {
+                    ForEach(viewModel.searchFilter.sorted(by: { $0.key < $1.key }), id: \.key) { (currencyCode, rate) in
+                        CurrencyCellView(imageAndName: currencyCode, currencyName: viewModel.countryCurrenciesManager.findCurrencyName(for: currencyCode) ?? "", amount: rate, selectedCurrency: viewModel.savedName)                        }
                 }
-                .onAppear {
-                    viewModel.fetchData()
-                }
+                .listStyle(.plain)
+                .listRowBackground(Color.clear)
+            }
+            .onAppear {
+                viewModel.fetchData()
             }
         }
     }
+}
 
 struct CurrencyView_Previews: PreviewProvider {
     static var previews: some View {
