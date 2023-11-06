d9ebdfef2908f825b98d3f36798d0b86ecf6cf6f
Vakhtang Daredzhanashvili <mymailbox14a@gmail.com>
2023-11-06 20:16:34 +0200

    delete trash from project

--- CurrencyConverterApp/RealmService/RealmService.swift
+++ CurrencyConverterApp/RealmService/RealmService.swift
@@ -12,7 +12,6 @@ protocol RealmServiceProtocol {
     func saveWallet(model: WalletModel)
     func loadWallet() async throws -> [WalletModel]
     func saveBid(model: BidModel)
-//    func loadBid() async throws -> [BidModel]
     func loadBid(completion: @escaping ([BidModel]) -> Void)
 }
 
@@ -48,19 +47,4 @@ final class RealmService: RealmServiceProtocol {
             completion(Array(realm.objects(BidModel.self)))
         }
     }
-//    @MainActor
-//    func saveBid(model: BidModel) {
-//            let realm = try! Realm()
-//            try! realm.write {
-//                model.isOpen.toggle()
-//                realm.add(model, update: .modified)
-//        }
-//    }
-//    
-//    @MainActor
-//    func loadBid() async throws -> [BidModel] {
-//        let realm = try await Realm()
-//        let item = realm.objects(BidModel.self)
-//        return Array(item)
-//    }
 }
